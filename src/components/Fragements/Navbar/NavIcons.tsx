b77c89298f083dc81acb2b648ad030832d1e4a78
faniadifrihandoko <faniadifrihandoko1@gmail.com>
2024-06-12 21:14:29 +0700

    fix: component navbar search

--- src/components/Fragements/Navbar/NavIcons.tsx
+++ src/components/Fragements/Navbar/NavIcons.tsx
@@ -16,20 +16,20 @@ const NavIcons: React.FC<NavIconsProps> = ({ setSidebarOpen, sidebarOpen }) => {
     <>
       <IoMenuOutline
         className="block md:hidden cursor-pointer hover:text-blue-600"
-        size={25}
+        size={30}
         onClick={() => setSidebarOpen(!sidebarOpen)}
       />
       <IoIosNotificationsOutline
         className="hover:text-blue-600 cursor-pointer"
-        size={25}
+        size={30}
       />
       <IoMdInformationCircleOutline
         className="cursor-pointer hover:text-blue-600"
-        size={25}
+        size={30}
       />
       <IoMdColorPalette
         className="cursor-pointer hover:text-blue-600"
-        size={25}
+        size={30}
       />
       <img
         className="h-5 w-5 p-4 md:h-9 md:w-9 md:p-0 rounded-full ring-2 ring-white bg-blue-800 flex justify-center items-center text-white text-sm font-semibold"
--- src/components/Fragements/Navbar/index.tsx
+++ src/components/Fragements/Navbar/index.tsx
@@ -13,16 +13,16 @@ const Navbar: React.FC<NavbarProps> = ({ sidebarOpen, setSidebarOpen }) => {
   const { setSearch } = useContext(SearchContext);
 
   return (
-    <div className="sticky top-0 z-30 flex flex-col md:flex-row md:justify-between md:items-center p-5 w-full transition-all duration-300 backdrop-blur-3xl bg-white/10">
+    <div className="sticky top-0 z-30 flex flex-col md:flex-row md:justify-between md:items-center p-6 w-full transition-all duration-300 backdrop-blur-3xl bg-white/10">
       <div className="md:mt-2">
-        <div className="breadcumbs-page text-sm text-gray-700">
+        <div className="breadcumbs-page text-sm font-medium text-gray-700">
           <h2>Pages / Data Tables</h2>
         </div>
         <div className="page-title text-3xl font-bold">
           <h1>Data Tables</h1>
         </div>
       </div>
-      <div className="flex items-center mt-4 bg-white dark:bg-black dark:text-white h-16 p-3 rounded-full gap-2 text-gray-500 shadow-md">
+      <div className="flex items-center mt-4 bg-white dark:bg-black dark:text-white h-16 p-3 rounded-full gap-2 text-gray-500 shadow-lg">
         <SearchBar onChange={setSearch} />
         <NavIcons setSidebarOpen={setSidebarOpen} sidebarOpen={sidebarOpen} />
       </div>
