46acd955f34db24e4b824c07d2a0444f8d15c8c5
faniadifrihandoko <faniadifrihandoko1@gmail.com>
2024-06-13 11:49:18 +0700

    fix: useContext search

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-import SearchContext from "./context/searchContext";
 import TablePages from "./pages/TablePage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage";
@@ -10,19 +8,16 @@ import RtlAdminPage from "./pages/RtlAdminPage";
 ("./pages/MainPages");
 
 function App() {
-  const [search, setSearch] = React.useState<string>("");
   return (
     <BrowserRouter>
-      <SearchContext.Provider value={{ search, setSearch }}>
-        <Routes>
-          <Route path="/" index element={<TablePages />} />
-          <Route path="/main-dashboard" element={<MainPage />} />
-          <Route path="/nft-marketplace" element={<MarketplacePage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/sign-in" element={<Signin />} />
-          <Route path="/rtl-admin" element={<RtlAdminPage />} />
-        </Routes>
-      </SearchContext.Provider>
+      <Routes>
+        <Route path="/" index element={<TablePages />} />
+        <Route path="/main-dashboard" element={<MainPage />} />
+        <Route path="/nft-marketplace" element={<MarketplacePage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/rtl-admin" element={<RtlAdminPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/pages/TablePage.tsx
+++ src/pages/TablePage.tsx
@@ -4,48 +4,52 @@ import dataTable, { DataTable } from "../mocks/dataTable";
 import Tabel from "../components/Fragements/Tabel";
 import { MdOutlineAddCircle } from "react-icons/md";
 import ModalForm from "../components/Fragements/ModalForm";
+import SearchContext from "../context/searchContext";
 
 export default function TablePages() {
   const [open, setOpen] = React.useState(false);
   const [data, setData] = React.useState<DataTable[]>(dataTable);
+  const [search, setSearch] = React.useState<string>("");
   const handleDelete = (id: number) => {
     setData(data.filter((item) => item.id !== id));
   };
 
   return (
-    <Layout>
-      <div
-        className={`w-full max-w-2xl md:max-w-full mx-auto ${
-          open ? "blur-background" : ""
-        }  bg-white shadow-lg rounded-xl border   border-gray-200 `}
-      >
-        <ModalForm
-          setOpen={setOpen}
-          open={open}
-          data={data}
-          setData={setData}
-        />
+    <SearchContext.Provider value={{ search, setSearch }}>
+      <Layout>
+        <div
+          className={`w-full max-w-2xl md:max-w-full mx-auto ${
+            open ? "blur-background" : ""
+          }  bg-white shadow-lg rounded-xl border   border-gray-200 `}
+        >
+          <ModalForm
+            setOpen={setOpen}
+            open={open}
+            data={data}
+            setData={setData}
+          />
 
-        <header className={` px-5 py-4   flex justify-between  items-center`}>
-          <h2 className="font-bold text-gray-800 text-2xl">
-            Development Table
-          </h2>
-          <div>
-            <div
-              onClick={() => setOpen(true)}
-              className="text-gray-800 text-sm mr-0 md:mr-8 py-2 px-2 flex gap-1 items-center bg-[#d9e2f6] rounded-md hover:bg-[#b4c2f4] cursor-pointer"
-            >
-              <MdOutlineAddCircle size={20} color="#2563EB" />
-              <h1 className="text-[#2563EB] font-semibold">Add</h1>
+          <header className={` px-5 py-4   flex justify-between  items-center`}>
+            <h2 className="font-bold text-gray-800 text-2xl">
+              Development Table
+            </h2>
+            <div>
+              <div
+                onClick={() => setOpen(true)}
+                className="text-gray-800 text-sm mr-0 md:mr-8 py-2 px-2 flex gap-1 items-center bg-[#d9e2f6] rounded-md hover:bg-[#b4c2f4] cursor-pointer"
+              >
+                <MdOutlineAddCircle size={20} color="#2563EB" />
+                <h1 className="text-[#2563EB] font-semibold">Add</h1>
+              </div>
+            </div>
+          </header>
+          <div className="p-3">
+            <div className="overflow-x-auto">
+              <Tabel data={data} onDelete={handleDelete} />
             </div>
           </div>
-        </header>
-        <div className="p-3">
-          <div className="overflow-x-auto">
-            <Tabel data={data} onDelete={handleDelete} />
-          </div>
         </div>
-      </div>
-    </Layout>
+      </Layout>
+    </SearchContext.Provider>
   );
 }
